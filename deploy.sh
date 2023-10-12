15839a313bb46815ab02d8a9ed7b4dbd59f33093
devclass1 <72622124+devclass1@users.noreply.github.com>
2023-10-12 20:57:17 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,11 @@
 
 # Stop the existing Spring Boot application
 sudo systemctl stop spring-boot-2-hello-world-1.0.2
-
+#Create a folder for application
+sudo mkdir /tmp/appdelploy
 # Copy the new JAR file from Jenkins workspace to the deployment directory
-cp /path/to/your/jenkins/workspace/test/target/your-application.jar /home/sysadmin/springbootapp
+#cp /path/to/your/jenkins/workspace/test/target/your-application.jar /home/sysadmin/springbootapp
+sudo cp /var/lib/jenkins/workspace/project10/target/spring-boot-2-hello-world-1.0.2-SNAPSHOT.jar /tmp/appdeploy
 
 # Start the Spring Boot application
-sudo systemctl start spring-boot-2-hello-world-1.0.2
+sudo java -jar spring-boot-2-hello-world-1.0.2-SNAPSHOT.jar
